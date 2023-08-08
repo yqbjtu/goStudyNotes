67dff23737b21f5731e982e4efd56d6cf38d0731
EricYang <yqbjtu@qq.com>
2023-08-08 21:10:46 +0800

    Create interfacedemo.go

---
+++ genericdemo/interfacedemo.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type MyInterface[T int|string] interface {
+	~int|float64
+	WriteOne(data T) T
+	ReadOne() T
+}
+type Note int
+
+func (n Note) WriteOne(one string) string {
+	return one
+}
+
+func (n Note) ReadOne() string {
+	return "small"
+}
+
+// 泛型接口，只能被当做类型参数来使用，无法被实例化。
+func add[T MyInterface[string]] (a, b T) T {
+	return a + b
+}
+
+func main() {
+	var a Note = 1
+	var b Note = 2
+	res := add(a, b)
+	fmt.Println(res)
+	res1 := res.WriteOne("123")
+	fmt.Println(res1)
+}
