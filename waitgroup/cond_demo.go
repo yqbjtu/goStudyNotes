5ef661af5f8980939023395bdbce580af329c2a5
EricYang <yqbjtu@163.com>
2023-09-07 20:56:06 +0800

    Create cond_demo.go

---
+++ waitgroup/cond_demo.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+var done = false
+
+func read(name string, c *sync.Cond) {
+	c.L.Lock()
+	log.Printf("%s has gotten lock", name)
+
+	for !done {
+		//调用 Wait 会自动释放锁 c.L，并挂起调用者所在的 goroutine
+		c.Wait()
+	}
+	log.Println(name, "starts reading")
+	c.L.Unlock()
+}
+
+func write(name string, c *sync.Cond) {
+	log.Println(name, "starts writing")
+	time.Sleep(time.Second)
+	c.L.Lock()
+	log.Printf("%s has gotten lock", name)
+
+	done = true
+	c.L.Unlock()
+	log.Println(name, "wakes all")
+	c.Broadcast()
+}
+
+func main() {
+	cond := sync.NewCond(&sync.Mutex{})
+
+	go read("reader1", cond)
+	go read("reader2", cond)
+	go read("reader3", cond)
+	write("writer", cond)
+
+	time.Sleep(time.Second * 3)
+}
