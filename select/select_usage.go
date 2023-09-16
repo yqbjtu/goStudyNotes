75e2fe084aae8df5162a6cf3d24e93c7b402a8e8
EricYang <yqbjtu@163.com>
2023-09-16 14:36:04 +0800

    Update select_usage.go

--- select/select_usage.go
+++ select/select_usage.go
@@ -16,6 +16,17 @@ func main() {
 	chanIsFull()
 	fmt.Println("----")
 
+	chanHasDataAfterSleep()
+	fmt.Println("----")
+
+	allExpressinIsCalc()
+	fmt.Println("----")
+
+	chanCannotReadOrWrite()
+	fmt.Println("----")
+}
+
+func chanCannotReadOrWrite() {
 	var ch1, ch2, ch3 chan int
 	var i1, i2 int
 
@@ -32,14 +43,13 @@ func main() {
 		} else {
 			fmt.Println("ch3 is closed")
 		}
-		/*如果注释default，并且上的三个channel都没有数据到达， 就会出现如下情况
-		fatal error: all goroutines are asleep - deadlock!
-		goroutine 1 [select]:
-		*/
+	/*如果注释default，并且上的三个channel都没有数据到达， 就会出现如下情况
+	fatal error: all goroutines are asleep - deadlock!
+	goroutine 1 [select]:
+	*/
 	default:
 		fmt.Println("no chan， execute default")
 	}
-
 }
 
 func chanCanRead() {
@@ -101,12 +111,85 @@ func chanIsFull() {
 		fmt.Println(" to send to chan is blocked, chan is full")
 	}
 }
+
+func chanHasDataAfterSleep() {
+	start := time.Now()
+	c := make(chan interface{})
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		time.Sleep(4 * time.Second)
+		close(c)
+	}()
+
+	go func() {
+
+		time.Sleep(3 * time.Second)
+		ch1 <- 3
+	}()
+
+	go func() {
+
+		time.Sleep(3 * time.Second)
+		ch2 <- 5
+	}()
+
+	fmt.Println("Blocking on read...")
+	select {
+	case <-c:
+		fmt.Printf("Unblocked %v later.\n", time.Since(start))
+	case <-ch1:
+		fmt.Printf("ch1 case...\n")
+	case <-ch2:
+		fmt.Printf("ch1 case...\n")
+	default:
+		fmt.Printf("default, for 3 chans have no data. they sleep some time\n")
+	}
+}
+
+func allExpressinIsCalc() {
+	var ch1 chan int
+	var ch2 chan int
+	var chs = []chan int{ch1, ch2}
+	var numbers = []int{1, 2, 3, 4, 5}
+
+	select {
+	case getChan(0, chs) <- getNumber(2, numbers):
+		fmt.Println("1th case is selected.")
+	case getChan(1, chs) <- getNumber(3, numbers):
+		fmt.Println("2th case is selected.")
+	default:
+		fmt.Println("default")
+	}
+}
+
+func getNumber(i int, numbers []int) int {
+	fmt.Printf("表达式 numbers[%d]\n", i)
+	return numbers[i]
+}
+
+func getChan(i int, chs []chan int) chan int {
+	fmt.Printf("表达式 chs[%d]\n", i)
+	return chs[i]
+}
+
 /*运行结果
 send data to ch2: 0
 ----
 timeout!
+	----
+	to send to chan is blocked, chan is full
 ----
- to send to chan is blocked, chan is full
+Blocking on read...
+default, for 3 chans have no data. they sleep some time
+----
+表 达 式  chs[0]
+表 达 式  numbers[2]
+表 达 式  chs[1]
+表 达 式  numbers[3]
+default
 ----
 no chan，  execute default
+----
 */
